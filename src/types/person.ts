a4626b24b6c360965464a57f99b97ea853e1c6db
Guipb1 <gui.pb1@hotmail.com>
2023-06-15 22:38:42 -0300

    feat(types):novas tipagens adicionadas

---
+++ src/types/person.ts
@@ -0,0 +1,15 @@
+export type Person = {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  skin_color: string;
+  films: string[];
+  vehicles: string[];
+  starships: string[];
+  species: string[];
+};
